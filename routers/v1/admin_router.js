c31074374de1da01cafbdfa15de00367b1b05d0b
Advait Maindalkar <maindalkar.advait@gmail.com>
2025-01-19 11:03:23 +0530

    updated admin router

--- routers/v1/admin_router.js
+++ routers/v1/admin_router.js
@@ -1,14 +1,38 @@
 const { Router } = require('express');
 const router = Router();
 
-router.get('/signup', (req, res) => {
+router.post('/signup', (req, res) => {
     const { email, name, password } = req.body;
     res.send({ status: 200, message: 'Signup successful' })
 })
 
-router.get('/signin', (req, res) => {
+router.post('/signin', (req, res) => {
     const { email, password } = req.body;
     res.send({ status: 200, message: 'Signin successful' })
+})
+
+router.post('/courses', (req, res) => {
+    const { name, description, price } = req.body;
+    res.send({ status: 200, message: 'Course created successfully' })
+})
+
+router.put('/courses/:id', (req, res) => {
+    const { name, description, price } = req.body;
+    res.send({ status: 200, message: 'Course updated successfully' })
+})
+
+router.delete('/courses/:id', (req, res) => {
+    const { name, description, price } = req.body;
+    res.send({ status: 200, message: 'Course deleted successfully' })
+})
+
+router.get('/courses/:id', (req, res) => {
+    const { id } = req.params;
+    res.send({ status: 200, message: 'Course fetched successfully' })
+})
+
+router.get('/courses', (req, res) => {
+    res.send({ status: 200, message: 'Courses fetched successfully' })
 })
 
 module.exports = router;
